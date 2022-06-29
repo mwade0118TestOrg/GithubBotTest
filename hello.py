1623d5b9f26198be0063e9307be7daba9a0b55b7
Matthew Wade <mattywa@amazon.com>
2022-06-29 16:17:11 -0700

    altered Random #2

--- hello.py
+++ hello.py
@@ -10,4 +10,4 @@ def zero(number_in):
 if __name__ == '__main__':
     print(f'100 plus one is {addOne(100)}')
     print(f'12 times 2 is {timesTwo(12)}')
-    print(f'Anything times 0 is {zero(999)}')
+    print(f'Anything times 0 is {zero(1200)}')
