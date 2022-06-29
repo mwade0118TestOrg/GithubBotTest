71b26c40031bd0c26f01e68e42868ec6222abb6a
Matthew Wade <mattywa@amazon.com>
2022-06-29 15:34:44 -0700

    Added Tests

--- hello.py
+++ hello.py
@@ -1,4 +1,13 @@
-name = 'Matthew'
-print(f'Hello {name}!')
-age = 19
-print(f'You are {age} years old!')
+def addOne(number_in):
+    return number_in + 1
+
+def timesTwo(number_in):
+    return number_in * 2
+
+def zero(number_in):
+    return 0
+
+if __name__ == '__main__':
+    print(f'100 plus one is {addOne(100)}')
+    print(f'12 times 2 is {timesTwo(12)}')
+    print(f'Anything times 0 is {zero(777)}')
