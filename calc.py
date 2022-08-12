2d5337b5208090b230b877ae0abd3927a0deb39b
Matthew <66389848+mwade0118@users.noreply.github.com>
2022-08-12 11:42:26 -0700

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,11 +1,11 @@
 def add(x,y):
-    return x + y
+    return y + x
 
 def subtract(x,y):
     return x - y
 
 def multiply(x, y):
-    return x * y
+    return y * x
 
 def divide(x,y):
     return x / y
